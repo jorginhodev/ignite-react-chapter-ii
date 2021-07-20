0d1b752c526d36e759f561de1eb94eece1bc9b03
Jorge Ramos <jorginhodeveloper@gmail.com>
2021-07-20 15:56:29 -0300

    feat: create modal styles

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from "react-modal";
+
+import closeImg from "../../assets/close.svg";
 import { Container } from "./styles";
 
 interface NewTransactionModalProps {
@@ -14,6 +16,13 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         overlayClassName="react-modal-overlay"
         className="react-modal-content"
       >
+        <button
+          type="button"
+          className="react-modal-close"
+          onClick={onRequestClose}
+        >
+          <img src={closeImg} alt="Fechar modal" />
+        </button>
         <Container>
           <h2>Cadastrar informação</h2>
 
